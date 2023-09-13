a3412780efce430392692746036fe574f6f1df18
khirasagar.maharana8100@gmail.com <khirasagar.maharana8100@gmail.com>
2023-09-13 20:45:59 +0530

    second commit

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // Get a reference to the button element by its id
 const addStudentButton = document.getElementById("addStudentButton");
+if (addStudentButton) {
 
-function addButtonClick() {
+// Add a click event listener to the button
+addStudentButton.addEventListener("click", function() {
   // Print "Student Added!" to the console when the button is clicked
-  console.log("Student Added!1234");
+  console.log("first");
+
+  window.location.href = "form.html";
+});
 }
+
+const back = document.getElementById("back");
+if(back){
+  back.addEventListener("click", function() {
+  console.log("first");
+  window.location.href = "index.html";
+  });
+
+}
+
