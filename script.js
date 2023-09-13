e1219f6ec57c9cfaa2fcb13811671da6732a0674
khirasagar.maharana8100@gmail.com <khirasagar.maharana8100@gmail.com>
2023-09-13 12:00:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get a reference to the button element by its id
+const addStudentButton = document.getElementById("addStudentButton");
+
+function addButtonClick() {
+  // Print "Student Added!" to the console when the button is clicked
+  console.log("Student Added!1234");
+}
