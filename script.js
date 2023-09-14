ac6acac8d3eea28cb2259e249c08b807eb298379
khirasagar.maharana8100@gmail.com <khirasagar.maharana8100@gmail.com>
2023-09-14 17:53:52 +0530

    commited for form submission updation

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-// Get a reference to the button element by its id
 const addStudentButton = document.getElementById("addStudentButton");
 if (addStudentButton) {
-
-// Add a click event listener to the button
-addStudentButton.addEventListener("click", function() {
-  // Print "Student Added!" to the console when the button is clicked
-  console.log("first");
-
+  addStudentButton.addEventListener("click", function() {
   window.location.href = "form.html";
-});
+  });
 }
 
 const back = document.getElementById("back");
@@ -17,17 +11,14 @@ if(back){
   console.log("first");
   window.location.href = "index.html";
   });
-
 }
 
 
-// Get a reference to the form element
 const contactForm = document.getElementById("contact_form");
-
 if (contactForm) {
   contactForm.addEventListener("submit", function (e) {
     e.preventDefault(); // Prevent the form from submitting normally
-
+    
     // Create an object to store the form values
     const formData = {
       fullName: contactForm.elements["first_name"].value,
